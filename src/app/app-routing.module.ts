2a385d371881cbd11d29497f6971a47a4ca6dcff
Juanse Franco <jsfc2199@gmail.com>
2024-07-17 17:31:19 -0500

    modularizando auth routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
 import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
 import { PagesRoutingModule } from './pages/pages-routing.module';
+import { AuthRoutingModule } from './auth/auth-routing.module';
 
 const routes: Routes = [
-
-
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: 'register',
-    component: RegisterComponent,
-  },
-
   {
     path: '**',
     component: NoPageFoundComponent,
@@ -24,7 +12,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), PagesRoutingModule],
+  imports: [RouterModule.forRoot(routes), PagesRoutingModule, AuthRoutingModule],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
