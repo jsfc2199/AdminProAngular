3931ed530a7f65e671efdb0a1155aae178b76af6
Juanse Franco <jsfc2199@gmail.com>
2024-07-18 18:43:12 -0500

    breadcrumbs con observable y parametros en las rutas

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class BreadcrumbsComponent {
 
+  private router = inject(Router)
+  titulo = ''
+
+  constructor(){
+   this.getArgumentosRuta()
+  }
+
+  getArgumentosRuta(){
+    this.router.events.pipe(
+      //filtramos para poder tener el titulo de la web
+      filter((event): event is ActivationEnd => event instanceof ActivationEnd),
+      filter((data: ActivationEnd) => data.snapshot.firstChild === null),
+      map((data:ActivationEnd) => data.snapshot.data)
+    ).subscribe(data => {
+      this.titulo = data['titulo']
+      document.title = `Admin Pro - ${this.titulo}`
+    })
+  }
 }
