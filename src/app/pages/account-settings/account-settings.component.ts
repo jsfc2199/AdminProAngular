d5f6dc759d124eb0cd6f31b7ed6d1b40da2db14b
Juanse Franco <jsfc2199@gmail.com>
2024-07-18 10:55:27 -0500

    añadiendo css a los colores que se escogen usando js vanilla

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -3,15 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-account-settings',
   templateUrl: './account-settings.component.html',
-  styleUrl: './account-settings.component.css'
+  styleUrl: './account-settings.component.css',
 })
 export class AccountSettingsComponent {
-//obtenemos el elemento del index <link href="assets/css/colors/default-dark.css" id="theme" rel="stylesheet">
-linkTheme = document.querySelector('#theme')
+  //obtenemos el elemento del index <link href="assets/css/colors/default-dark.css" id="theme" rel="stylesheet">
+  linkTheme = document.querySelector('#theme');
+  links!: NodeListOf<Element>;
 
-  changeTheme(theme:string){
-    const url = `assets/css/colors/${theme}.css`
-    this.linkTheme?.setAttribute('href', url)
-    localStorage.setItem('theme', url)
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.links = document.querySelectorAll('.selector');
+    this.checkCurrentTheme();
+  }
+
+  changeTheme(theme: string) {
+    const url = `assets/css/colors/${theme}.css`;
+    this.linkTheme?.setAttribute('href', url);
+    localStorage.setItem('theme', url);
+    this.checkCurrentTheme();
+  }
+
+  checkCurrentTheme() {
+    this.links.forEach((item) => {
+      item.classList.remove('working');
+      const bthTheme = item.getAttribute('data-theme');
+      const btnThemeUrl = `assets/css/colors/${bthTheme}.css`;
+
+      const currentTheme = this.linkTheme?.getAttribute('href');
+
+      if (btnThemeUrl === currentTheme) {
+        item.classList.add('working');
+      }
+    });
   }
 }
