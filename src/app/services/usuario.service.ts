4e60b258e3cd789148816ffe20e45280289302b4
Juanse Franco <jsfc2199@gmail.com>
2024-07-22 21:13:33 -0500

    guardar token en local storage en login y registro

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -3,6 +3,7 @@ import { inject, Injectable } from '@angular/core';
 import { RegisterForm } from '../auth/interfaces/register-form.interface';
 import { environments } from '../../environments/environment';
 import { LoginForm } from '../auth/interfaces/login-form.interface';
+import { tap } from 'rxjs';
 
 const baseUrl = environments.base_url
 
@@ -15,9 +16,19 @@ export class UsuarioService {
 
   crearUsuario(formData: RegisterForm){
     return this.http.post(`${baseUrl}/usuarios`, formData)
+    .pipe(
+      tap((resp: any) => {
+        localStorage.setItem('token', resp.token)
+      })
+    )
   }
 
   login(formData: LoginForm){
     return this.http.post(`${baseUrl}/login`, formData)
+    .pipe(
+      tap((resp: any) => {
+        localStorage.setItem('token', resp.token)
+      })
+    )
   }
 }
