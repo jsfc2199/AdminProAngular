0dad88b6bd61958cad401ac591f23c71466b4c92
Juanse Franco <jsfc2199@gmail.com>
2024-07-25 19:05:24 -0500

    medico service con su get y cambiar imagen

--- src/app/pages/mantenimientos/medicos/medicos.component.ts
+++ src/app/pages/mantenimientos/medicos/medicos.component.ts
@@ -1,9 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MedicosService } from '../../../services/medicos.service';
+import { Medicos } from '../../../models/medicos.model';
+import { ModalImagenService } from '../../../services/modal-imagen.service';
+import { delay, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-medicos',
   templateUrl: './medicos.component.html',
 })
 export class MedicosComponent {
+  private medicosService = inject(MedicosService);
+  private modalService = inject(ModalImagenService);
 
+  public modalSubscription = new Subscription();
+
+  public cargando: boolean = true;
+  public medicos: Medicos[] = [];
+
+  ngOnInit(): void {    
+    this.cargarMedicos()
+
+    this.modalSubscription = this.modalService.nuevaImage
+      .pipe(
+        delay(100) //como la carga es muy rápida esperamos un poco para que el servidor que se pueda refrescar
+      )
+      .subscribe((img) => this.cargarMedicos());
+  }
+
+  cargarMedicos() {
+    this.cargando = true;
+
+    this.medicosService.cargarMedicos().subscribe((medicos) => {
+      this.medicos = medicos
+      this.cargando = false
+    });
+  }
+
+  abrirModal(medico: Medicos){
+    this.modalService.abrirModal('medicos', medico._id!, medico.img);
+  }
 }
--- src/app/services/medicos.service.ts
+++ src/app/services/medicos.service.ts
@@ -1,9 +1,49 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environments } from '../../environments/environment';
+import { map } from 'rxjs';
+import { Medicos } from '../models/medicos.model';
 
+const baseUrl = environments.base_url;
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MedicosService {
+  constructor() {}
 
-  constructor() { }
+  private http = inject(HttpClient);
+
+  cargarMedicos() {
+    const url = `${baseUrl}/medicos`;
+    return this.http
+      .get<{ ok: boolean; medicos: Medicos[] }>(url, this.headers)
+      .pipe(map((resp: { ok: boolean; medicos: Medicos[] }) => resp.medicos));
+  }
+
+  crearMedico(medico: string) {
+    const url = `${baseUrl}/medicos`;
+    return this.http.post(url, medico, this.headers);
+  }
+
+  actualizarMedico(medico: Medicos) {
+    const url = `${baseUrl}/medicos/${medico._id}`;
+    return this.http.put(url, medico, this.headers);
+  }
+
+  eliminarMedicos(_id: string) {
+    const url = `${baseUrl}/medicos/${_id}`;
+    return this.http.delete(url, this.headers);
+  }
+
+  get headers() {
+    return {
+      headers: {
+        'x-token': this.token,
+      },
+    };
+  }
+
+  get token() {
+    return localStorage.getItem('token') ?? '';
+  }
 }
