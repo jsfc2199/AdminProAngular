c8e99b2c6faa2379fa982bf361e882523e77ffbc
Juanse Franco <jsfc2199@gmail.com>
2024-07-25 20:11:14 -0500

    preparar formulario para añadir un medico con un hospital seleccionado

--- src/app/pages/mantenimientos/medicos/medico.component.ts
+++ src/app/pages/mantenimientos/medicos/medico.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HospitalService } from '../../../services/hospital.service';
+import { Hospital } from '../../../models/hospital.model';
 
 @Component({
   selector: 'app-medico',
@@ -6,4 +9,31 @@ import { Component } from '@angular/core';
 })
 export class MedicoComponent {
 
+  private fb = inject(FormBuilder)
+  private hospitalService = inject(HospitalService)
+
+  public medicoForm: FormGroup
+  public hospitales: Hospital[]= []
+
+  constructor(){
+    this.medicoForm = this.fb.group({
+      nombre: ['Juan', [Validators.required]],
+      hospital: ['', [Validators.required]]
+    })
+  }
+
+  ngOnInit(): void {
+    this.cargarHospitales()
+  }
+
+  cargarHospitales(){
+    this.hospitalService.cargarHospitales()
+    .subscribe((hospitales: Hospital[])=> {
+      this.hospitales = hospitales
+    })
+  }
+
+  guardarMedico(){
+    console.log(this.medicoForm.value)
+  }
 }
